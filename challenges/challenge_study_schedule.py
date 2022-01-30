019de53b6700051373d5f5afb0cd01435bf4d59e
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2022-01-30 19:12:29 -0300

    req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if type(target_time) is not int:
+        return None
+    result = 0
+
+    for x in permanence_period:
+        if type(x[0]) is not int or type(x[1]) is not int:
+            return None
+
+        if target_time >= x[0] and target_time <= x[1]:
+            result += 1
+
+    return result
