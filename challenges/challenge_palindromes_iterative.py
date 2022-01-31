5d3129745547bbf81cf38daac5fd63337fa0e6c0
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2022-01-31 04:31:19 -0300

    req5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word or word[0] != word[-1]:
+        return False
+    if len(word) == 1 or len(word) == 2 and word[0] == word[1]:
+        return True
+
+    return word == word[::-1]
