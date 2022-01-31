1e912542adc0e09840578d04bf8e8f9ef7f189b6
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2022-01-31 04:01:49 -0300

    req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,20 @@
+def revert(word, low_index, high_index):
+    if len(word) == 1:
+        return word
+
+    else:
+        return revert(
+            word[high_index], low_index, high_index
+        ) + revert(word[:-1], low_index, high_index - 1)
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    reverted = revert(word, low_index, high_index)
+
+    if reverted == word:
+        return True
+    else:
+        return False
