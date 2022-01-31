99c06e7960f808b07a626fb035b17e1712ee18bc
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2022-01-31 04:47:25 -0300

    fix

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -17,3 +17,7 @@ def sorting(string):
 
 def is_anagram(first_string, second_string):
     return sorting(first_string) == sorting(second_string)
+
+# Obtive ajuda do meu colega Ivan Rafael nos requisitos 3-5
+# Valeu, Ivan!
+# PR Ivan: https://github.com/tryber/sd-010-b-project-algorithms/pull/78
