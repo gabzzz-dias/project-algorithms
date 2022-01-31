8a7772578708680390287b5af4b586f75a187ffd
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2022-01-31 04:41:40 -0300

    fix

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,13 +1,10 @@
 def find_duplicate(nums):
     if len(nums) <= 1:
         return False
-
     for x in nums:
         if type(x) != int or x < 0:
             return False
-
         result = [x for x in nums if nums.count(x) > 1]
-
         if len(result) > 0:
             return result[0]
         else:
