72c358afd6c42afb888bd6544d63bd6c4a5c77de
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2022-01-31 04:07:31 -0300

    req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,19 @@
+def sorting(string):
+    arr = list(string)
+    result = []
+
+    while arr:
+        first = arr[0]
+
+        for x in arr:
+            if x < first:
+                first = x
+
+        result.append(first)
+        arr.remove(first)
+
+    return result
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    return sorting(first_string) == sorting(second_string)
